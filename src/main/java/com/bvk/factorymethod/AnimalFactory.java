e113cb3c6b25f2cba83012d9ffe65c9188c1b4ad
Munifatir Rizqi Aziz <munifatirra@gmail.com>
2024-02-01 13:35:51 +0700

    create design pattern project

--- src/main/java/com/bvk/factorymethod/AnimalFactory.java
+++ src/main/java/com/bvk/factorymethod/AnimalFactory.java
@@ -3,14 +3,15 @@ package com.bvk.factorymethod;
 public class AnimalFactory {
 
     public static Animal create(String type) {
-        if (type.equals("tiger")) {
-            return new Tiger();
-        }else if (type.equals("cat")) {
-            return new Cat();
-        }else if (type.equals("dog")) {
-            return new Dog();
-        }else {
-            return new Cat();
+        switch (type) {
+            case "tiger":
+                return new Tiger();
+            case "cat":
+                return new Cat();
+            case "dog":
+                return new Dog();
+            default:
+                throw new RuntimeException("type not found");
         }
     }
 
